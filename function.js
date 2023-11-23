b7d28553c91da83ee285fb66d9741b9ddffbba9f
Braian <braiannickolas12@gmail.com>
2023-11-23 19:52:19 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,10 +1,16 @@
-function createPhrases() {
-  console.log('Estudar e muito bom')
-  console.log('paciencia e persistencia')
-  console.log('Revisao e mae do aprendizado')
+// function expression
+//funcgtion anonymous
+
+// parametros ( parameters)
+const sum = function (number1, number2) {
+  total = (number1 + number2)
+  return total
 }
 
-// executar a funcao
-// rodar, chamar, invocar
-// execute, run, call, invoke
-createPhrases()
+
+let number1 = 34
+let number2 = 25
+
+console.log(`o numero 1 e ${number1}`)
+console.log(`o numero 2 e ${number2}`)
+console.log(`a soma e ${sum(number1, number2)}`)
