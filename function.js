67df74f6c70b5ca915c756bbe3f0b86c83818b9f
Braian <braiannickolas12@gmail.com>
2023-11-23 08:55:46 -0300

    Funcoes
    Aula115

---
+++ function.js
@@ -0,0 +1,10 @@
+function createPhrases() {
+  console.log('Estudar e muito bom')
+  console.log('paciencia e persistencia')
+  console.log('Revisao e mae do aprendizado')
+}
+
+// executar a funcao
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
