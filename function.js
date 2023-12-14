f5fcf17462c3651f1a3cc28ddca7f94555f184b8
Braian <braiannickolas12@gmail.com>
2023-12-14 16:34:01 -0300

    Encontrando palavras em frase
    aula131

--- function.js
+++ function.js
@@ -1,5 +1,2 @@
 let phrase = "eu quero viver o amor!"
-let myArray = phrase.split(" ")
-console.log(myArray)
-let phraseWithUnderscore = myArray.join("_")
-console.log(phraseWithUnderscore.toUpperCase())
+console.log(phrase.includes("Amor"))
---
+++ function7.js
@@ -0,0 +1,5 @@
+let phrase = "eu quero viver o amor!"
+let myArray = phrase.split(" ")
+console.log(myArray)
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore.toUpperCase())
