ef3e597d9d1ba9e1787b4ca39c119320094602e5
Braian <braiannickolas12@gmail.com>
2023-12-14 16:46:15 -0300

    manipulando arrays
    aula135

--- function.js
+++ function.js
@@ -1,2 +1,12 @@
-let word = "manipulacao"
-console.log(Array.from(word))
+let techs = ["html", "css", "js"]
+console.log(techs.push("nodejs"))
+techs.unshift("sql")
+techs.pop()
+techs.shift()
+//console.log(techs.slice(1, 3))
+//techs.splice(1, 3)
+let index = techs.indexOf('css')
+techs.splice(2)
+
+
+console.log(techs)
