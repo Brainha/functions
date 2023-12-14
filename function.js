f9e8b4be0c089e38bd4c977ad330eb0874075112
Braian <braiannickolas12@gmail.com>
2023-12-14 16:19:12 -0300

    .lenght na pratica
    aula 127

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-let string = "123"
-console.log(Number(string))
-let number = 321
-console.log(String(number))
+let word = "paralelepipedo"
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)
